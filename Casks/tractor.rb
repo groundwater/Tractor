2ea194174e43cb9db37da6d9225419ed3d356735
Bender <bender@Benders-Virtual-Machine.local>
2026-05-12 14:37:59 -0700

    Finalize 0.2.0 release artifacts

--- Casks/tractor.rb
+++ Casks/tractor.rb
@@ -1,6 +1,6 @@
 cask "tractor" do
   version "0.2.0"
-  sha256 "REPLACE_WITH_0_2_0_PKG_SHA256"
+  sha256 "4eb3d7ddba110f528d2abde5326322edcda900405bd3e1ae5b65b7d186fca524"
 
   url "https://github.com/groundwater/Tractor/releases/download/v#{version}/Tractor-#{version}.pkg",
       verified: "github.com/groundwater/Tractor/"
@@ -27,13 +27,15 @@ cask "tractor" do
   ]
 
   caveats <<~EOS
-    Tractor can activate two system extensions after install:
+    Tractor can activate three privileged components after install:
       1. Endpoint Security (required for `tractor trace`)
       2. Network Extension (optional, for `--net` / `--mitm`)
+      3. Certificate Root (optional, for `--mitm`)
 
     Explicit post-install steps:
 
       sudo tractor activate endpoint-security
       sudo tractor activate network-extension
+      sudo tractor activate certificate-root
   EOS
 end
